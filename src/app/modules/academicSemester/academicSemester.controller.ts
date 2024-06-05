6e1a0ade554108518a927bbcad0de4d2ce005181
MAHossain1 <mahpro110@gmail.com>
2024-06-05 17:43:24 +0600

    Retrieved all academicSemesters

--- src/app/modules/academicSemester/academicSemester.controller.ts
+++ src/app/modules/academicSemester/academicSemester.controller.ts
@@ -12,6 +12,16 @@ const insertIntoDB = async (req: Request, res: Response, next: NextFunction) => 
   }
 };
 
+const getAllFromDB = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const result = await AcademicSemesterService.getAllFromDB(req);
+    sendResponse(res, result);
+  } catch (error) {
+    next(error);
+  }
+};
+
 export const AcademicSemesterController = {
-  insertIntoDB
+  insertIntoDB,
+  getAllFromDB
 };
